4829464e8ba40be666b29debbb51e563efd57e89
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-01-03 10:41:00 +0530

    implemented a link-lisst based queue

---
+++ queues/link_list_based_queue.py
@@ -0,0 +1,60 @@
+
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+class Queue:
+    def __init__(self) -> None:
+        self.front = self.rear = None
+
+    def enqueue(self, item) -> None:
+        temp = Node(item)
+
+        # if queue is empty, then new node is front and rear both
+        if self.rear is None:
+            self.front = self.rear = temp
+            return
+
+        # add the new node at the end of queue and change rear
+        self.rear.next = temp
+        self.rear = temp
+
+    def dequeue(self) -> object:
+        # if queue is empty, then return None
+        if self.front is None:
+            return None
+
+        # store previous front and move front one node ahead
+        temp = self.front
+        self.front = temp.next
+
+        # if front becomes None, then change rear also as None
+        if self.front is None:
+            self.rear = None
+        return temp.data
+    
+    def __str__(self) -> str:
+        if self.front is None:
+            return "Queue is empty."
+        else:
+            temp = self.front
+            result = ""
+            while temp is not None:
+                result += str(temp.data) + " "
+                temp = temp.next
+            return result
+        
+        
+if __name__ == "__main__":
+    queue = Queue()
+
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+
+    print(queue)
+
+    queue.dequeue()
+
+    print(queue)
